2d874894df124c255b2d49ac7482ea56891ae88c
paola422 <paola.canchala27@gmail.com>
2021-06-21 16:56:50 -0500

    prints Holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* main - the function main be  generates a random number  a validate
+*
+* Return: Always 0 (Sucess)
+*/
+int main(void)
+{
+_putchar ('H');
+_putchar ('o');
+_putchar ('l');
+_putchar ('b');
+_putchar ('e');
+_putchar ('r');
+_putchar ('t');
+_putchar ('o');
+_putchar ('n');
+_putchar('\n');
+return (0);
+}
