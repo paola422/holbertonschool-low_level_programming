b6177981b6340e543bae127007c38defc9bc44f0
paola422 <paola.canchala27@gmail.com>
2021-06-22 20:05:46 -0700

    Print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* print_last-digit - Print last number
+* @last: digit
+* Return: number
+*/
+int print_last_digit(int last)
+{
+int num;
+num = last % 10;
+if (num < 0)
+{
+num = num * -1;
+}
+_putchar(num + '0');
+return (num);
+}
