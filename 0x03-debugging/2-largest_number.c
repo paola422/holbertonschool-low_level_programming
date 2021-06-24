b229257145a23944400cd559f5b4f6b1591cc586
paola422 <paola.canchala27@gmail.com>
2021-06-24 10:43:39 -0700

    Largest

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 * largest_number - returns the largest of 3 numbers
 * @a: first integer
@@ -7,48 +6,24 @@
 * @c: third integer
 * Return: largest number
 */
-
-
 int largest_number(int a, int b, int c)
-
 {
-
 int largest;
-
-
 if (a > b && b > c)
-
 {
-
 largest = a;
-
 }
-
 else if (b > a && b > c)
-
 {
-
 largest = b;
-
 }
-
 else if (a == b)
-
 {
-
 largest = a;
-
 }
-
 else
-
 {
-
 largest = c;
-
 }
-
-
 return (largest);
-
 }
