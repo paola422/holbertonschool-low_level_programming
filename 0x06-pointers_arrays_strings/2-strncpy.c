58e28dcd855abce937ad53da800307c8fcb7a176
paola422 <paola.canchala27@gmail.com>
2021-07-02 17:38:32 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+*_strncpy- Function that copies a string
+*@dest:copy
+*@src: copy
+*@n: number of bytes
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while (src[i] != '\0' && i < n)
+{
+*(dest + i) = *(src + i);
+i++;
+}
+while (i < n)
+{
+*(dest + i) = '\0';
+i++;
+}
+return (dest);
+}
