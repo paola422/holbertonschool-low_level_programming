05da3b3bf260478da8c1b86f9de58b5ae221c4ec
paola422 <paola.canchala27@gmail.com>
2021-07-05 18:27:05 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+* *_memset - function that fills memory with a constant byte
+* @s: memory filled
+* @b: byte
+* @n: fill
+* Return: Pointer to the memory area s
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+return (s);
+}
