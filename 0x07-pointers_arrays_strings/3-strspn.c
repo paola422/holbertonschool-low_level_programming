c1cce92bb2461c2b7c870fd94dc0402f88c858bf
paola422 <paola.canchala27@gmail.com>
2021-07-05 20:38:04 -0700

    Substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* _strspn - function that gets the length of a prefix substring
+* @s: string
+* @accept: string
+* Return: bytes of s in accept
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a, b;
+
+	for (a = 0; s[a]; a++)
+
+	{
+		for (b = 0; accept[b]; b++)
+
+		{
+
+			if (s[a] == accept[b])
+				break;
+
+		}
+
+		if (!accept[b])
+			break;
+
+	}
+
+return (a);
+}
