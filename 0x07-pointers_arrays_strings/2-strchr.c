904a4752b14631f57ebd0be9f3244bbd9ffb3490
paola422 <paola.canchala27@gmail.com>
2021-07-05 22:54:47 -0700

    Correction en if

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -6,13 +6,20 @@
 * Return: Pointer to spot in s with c or NULL
 */
 char *_strchr(char *s, char c)
+
 {
 	int i;
 
 	for (i = 0; *(s + i); i++)
+
 	{
+
 	if (*(s + i) == c)
 		return (s + i);
 	}
+
+	if (*(s + i) == c)
+		return (s + i);
+
 return ('\0');
 }
