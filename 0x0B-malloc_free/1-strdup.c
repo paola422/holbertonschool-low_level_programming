06d4ebfd5327dfc06f0be78805b099de6d741abf
paola422 <paola.canchala27@gmail.com>
2021-07-15 03:27:52 -0700

    correction

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
-* _strdup - function that returns a pointer to a newly allocated space in memory
+* _strdup - function pointer to a newly allocated space in memory
 * @str: string.
 * Return: pointer to the duplicated string. It returns NULL
 */
