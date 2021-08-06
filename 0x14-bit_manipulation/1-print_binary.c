8d9cd01da84e18ccddeeee6d950f721e532071bd
paola422 <paola.canchala27@gmail.com>
2021-08-05 21:50:51 -0500

    Change the character the firts file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,41 +1,20 @@
 #include "main.h"
 /**
- * _pow - calculates a ^ b
- * @a: base
- * @b: power
- * Return: int
- */
-unsigned long int _pow(unsigned int a, unsigned int b)
-{
-	unsigned long int re;
-	unsigned int i;
-
-	re = 1;
-
-	for (i = 1; i <= b; i++)
-		re *= a;
-	return (re);
-}
-/**
- * print_binary - prints a number in binary notation
- * @n: int
- * Return: void
+ *print_binary - Function that prints the binary representation of a number.
+ *@n: number to comnvert to binary
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int a;
-	char f = 0;
-
-	a = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (a != 0)
+	if (n == 0)
 	{
-		if ((n & a) == a)
-		{
-			f = 1;
-			_putchar('1');
-		}
-		else if (f == 1 || a == 1)
-			_putchar('0');
-		a >>= 1;
+		putchar('0');
+		return;
 	}
+	else if (n == 1)
+	{
+		putchar ('1');
+		return;
+	}
+	print_binary(n >> 1);
+	putchar('0' + (n & 1));
 }
